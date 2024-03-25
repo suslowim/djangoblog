d098a1cc0225b9fbef88640f67d9c7ffe47bffea
Max Suslowicz <max@suslowicz.co>
2024-03-25 18:34:45 +0000

    Added post model

--- djangoblog/blog/views.py
+++ djangoblog/blog/views.py
@@ -1,27 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-posts = [
-    {
-        "author": "MaxS",
-        "title": "Blog Post 1",
-        "content": "This is my first post!",
-        "date_posted": "January 1, 2024"
-    },
-{
-        "author": "AndrewT",
-        "title": "Blog Post 2",
-        "content": "Second Post",
-        "date_posted": "January 3, 2024"
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        "posts": posts
+        "posts": Post.objects.all()
     }
     return render(request, template_name='blog/home.html', context=context)
 
