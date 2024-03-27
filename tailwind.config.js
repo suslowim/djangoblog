c35d26623bd1e9a4c49edfc03848c1610eeb4930
Max Suslowicz <max@suslowicz.co>
2024-03-27 21:32:37 +0000

    Tailwind first pass

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./djangoblog/**/templates/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
